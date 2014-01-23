6fd6c165390021aa9a544b672532b6f3f02c39a4
Nitix <guillaume.pierson.gp@gmail.com>
2014-01-23 15:37:27 +0100

    fix panier

--- src/Controller/RestoController.php
+++ src/Controller/RestoController.php
@@ -1,53 +1,52 @@
 <?php
 
-class RestoController extends Controller{
+class RestoController extends Controller {
 
-	protected static $actions = array(
-		    "theme"   => "listeTheme" ,
-		    "resto"  => "listeResto",
-		    "plats"  => "listePlats",
-		    "ajoutPanier" => "ajoutPanier"
-	);
+	protected static $actions = array("theme" => "listeTheme", "resto" => "listeResto", "plats" => "listePlats", "panier" => "panier");
 
-	public static function listeTheme(){
+	public static function listeTheme() {
 		$alltheme = Theme::findAll();
 		$v = new RestoVue($alltheme);
-		$v->displayPage("listeTheme"); 
+		$v -> displayPage("listeTheme");
 	}
 
-	public static function listeResto(){
-		$res=$_GET["id"];
+	public static function listeResto() {
+		$res = $_GET["id"];
 		$restoByTheme = Restaurant::findByTheme($res);
 		$v = new RestoVue($restoByTheme);
-		$v->displayPage("listeResto");
+		$v -> displayPage("listeResto");
 	}
 
-
-	public static function listePlats(){
-		$res=$_GET["id"];
-		$data['plats'] = Plats::findByResto($res);	
-		$data['resto'] = Restaurant::findById($res);	
+	public static function listePlats() {
+		$res = $_GET["id"];
+		$data['plats'] = Plats::findByResto($res);
+		$data['resto'] = Restaurant::findById($res);
 		$v = new RestoVue($data);
-		$v->displayPage("listePlats");
+		$v -> displayPage("listePlats");
 	}
 
 	// action par defaut de la page visiteur
-	public static function home(){
+	public static function home() {
 		self::listeTheme();
 	}
-	
-	public static function ajoutPanier(){
-		$plats_resto = Plats::findByResto($_POST['resto']);
-		foreach($plats_resto as $plat){
-			if($_POST[$plat->__get('id')] > 0)
-				Panier::add($plat->__get('id'), $_POST[$plat->__get('id')]);
-		}
-		self::panier();
-	}
-	
-	public static function panier(){
-		
-	}
-}
 
+	public static function ajoutPanier() {
+		$plats_resto = Plats::findByResto($_POST['resto']);
+		foreach ($plats_resto as $plat) {
+			if ($_POST[$plat -> __get('id')] > 0)
+				Panier::add($plat -> __get('id'), $_POST[$plat -> __get('id')]);
+		}
+	}
+
+	public static function panier() {		
+		if (isset($_POST['resto'])) {
+			$plats_resto = Plats::findByResto($_POST['resto']);
+			foreach ($plats_resto as $plat) {
+				if ($_POST[$plat -> __get('id')] > 0)
+					Panier::add($plat -> __get('id'), $_POST[$plat -> __get('id')]);
+			}
+		}
+	}
+
+}
 ?>
