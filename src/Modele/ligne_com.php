31a6c520525049ada29779557472ec16821527c0
Krack54 <teragon54@hotmail.fr>
2014-01-23 14:17:58 +0100

    lign com

--- src/Modele/ligne_com.php
+++ src/Modele/ligne_com.php
@@ -1,20 +1,19 @@
 <?php
 
-class panier{
+class ligne_com{
 
-	private $idutil;
+	private $idcom;
 	private $idplats;
 	private $nb;
 
-
-  public function __construct() {
+		  public function __construct() {
     // rien à faire
   }
 
 
   
   public function __toString() {
-        return "idutil : ". $this->idutil . "
+        return "idcom : ". $this->idcom . "
 				  idplats : ". $this->idplats  ."
 				  nb : ". $this->nb;
   }
@@ -45,14 +44,14 @@ class panier{
     $c = Base::getConnection();
     
 
-    $query = $c->prepare( "UPDATE panier set nb=?
-				                   where idutil = ?, idplats=?");
+    $query = $c->prepare( "UPDATE ligne_com set nb=?
+				                   where idcom = ?, idplats=?");
     
     /* 
      * liaison des paramêtres : 
     */
     $query->bindParam (1, $this->nb, PDO::PARAM_STR);
-    $query->bindParam (2, $this->idutil, PDO::PARAM_STR); 
+    $query->bindParam (2, $this->idcom, PDO::PARAM_STR); 
     $query->bindParam (3, $this->idplats, PDO::PARAM_INT);
 
     /*
@@ -73,11 +72,11 @@ class panier{
    */
   public function delete() { 
     
-    if (!isset($this->idutil) || !isset($this->idplats) ) {
+    if (!isset($this->idcom) || !isset($this->idplats) ) {
       throw new Exception(__CLASS__ . ": Primary Key undefined : cannot update");
     } 
     $c = Base::getConnection(); 
-    $query = $c->prepare( "DELETE from panier where idutil=? ,idplats=?");
+    $query = $c->prepare( "DELETE from ligne_com where idcom=? ,idplats=?");
      //liaison des paramêtres : 
 
     $query->bindParam (1, $this->id_util, PDO::PARAM_INT);
@@ -101,10 +100,10 @@ class panier{
       throw new Exception(__CLASS__ . ": Primary Key undefined : cannot update");
     } 
     $c = Base::getConnection();
-    $query = $c->prepare("INSERT INTO panier (nb, idutil, idplats) VALUES ( ?, ? , ? )");
+    $query = $c->prepare("INSERT INTO ligne_com (nb, idcom, idplats) VALUES ( ?, ? , ? )");
 
     $query->bindParam (1, $this->nb, PDO::PARAM_STR);
-    $query->bindParam (2, $this->idutil, PDO::PARAM_STR); 
+    $query->bindParam (2, $this->idcom, PDO::PARAM_STR); 
     $query->bindParam (3, $this->idplats, PDO::PARAM_INT);
     
     $query->execute();
@@ -121,19 +120,19 @@ class panier{
    *  
    *   @static
    *   @param integer $id OID to find
-   *   @return panier renvoie un objet de type panier
+   *   @return ligne_com renvoie un objet de type ligne_com
    */
-    public static function findByIdUtil($id) {  
+    public static function findByidcom($id) {  
 
       $c = Base::getConnection();
-      $query = $c->prepare("SELECT * from panier where idutil =?") ;
+      $query = $c->prepare("SELECT * from ligne_com where idcom =?") ;
     $query->bindParam (1, $id, PDO::PARAM_INT); 
       $dbres = $query->execute();
       $bil = null;
 
       if($d = $query->fetch(PDO::FETCH_BOTH)){
-        $bil = new panier();
-        $bil->idutil=$d['idutil'];
+        $bil = new ligne_com();
+        $bil->idcom=$d['idcom'];
     	$bil->idplats=$d['idplats'];
     	$bil->nb=$d['nb'];
       }
@@ -145,22 +144,22 @@ class panier{
     /**
      *   Finder All
      *
-     *   Renvoie toutes les lignes de la table panier
+     *   Renvoie toutes les lignes de la table ligne_com
      *   sous la forme d'un tableau d'objet
      *  
      *   @static
-     *   @return Array renvoie un tableau de panier
+     *   @return Array renvoie un tableau de ligne_com
      */
     
     public static function findAll() { 
       
       $c = Base::getConnection();
-      $reponse = $c->prepare("SELECT * FROM panier");
+      $reponse = $c->prepare("SELECT * FROM ligne_com");
       $dbres = $reponse->execute(); 
       while ($d = $reponse->fetch(PDO::FETCH_BOTH)){
 
-		$bil = new panier();
-		$bil->idutil=$d['idutil'];
+		$bil = new ligne_com();
+		$bil->idcom=$d['idcom'];
 		$bil->idplats=$d['idplats'];
 		$bil->nb=$d['nb'];
 
